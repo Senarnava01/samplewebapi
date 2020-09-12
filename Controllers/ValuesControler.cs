c22d81e5ef42948071b572ece78ffd15dea87abc
Senarnava01 <malay1780@gmail.com>
2020-09-12 10:55:55 +0530

    Second commit

---
+++ Controllers/ValuesControler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesControler : ControllerBase
+    {
+        [HttpGet]
+        [Route("getVal")]
+
+        public string getdetails()
+        {
+            return "My name is Arnabha";
+        }
+    }
+}
